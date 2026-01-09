12c4753fefd44f70152f54df5ae2e2f76f96fa52
fahimtakin <fahim.tanzil@hotmail.com>
2026-01-08 21:53:02 -0400

    add routes

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,21 @@
 const express = require("express");
 require("dotenv").config();
 require("./db");
+
 const User = require("./models/User");
+
+//routes mounted
+
+const registerRoute = require("./routes/register");
 
 const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/users", (req, res) => {
   res.send("Hello World!");
 });
 
@@ -18,6 +27,8 @@ app.post("/users", async (req, res) => {
     res.status(400).json({ error: err.message });
   }
 });
+
+app.use("/", registerRoute);
 
 app.listen(process.env.PORT || 3000, () => {
   console.log(`Server is running on port ${process.env.PORT || 3000}`);
